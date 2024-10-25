19d7f47b8247612733db4c7122ec07a15dac5db0
DmitriiKolotuhin <haivenspiret@gmail.com>
2024-10-25 19:17:02 +0300

    Task04

---
+++ Task04/guess-number/src/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Haiven\GuessNumber;
+
+use Haiven\GuessNumber\View;
+use Haiven\GuessNumber\Game;
+
+class Controller {
+    public static function startGame($maxNumber, $maxAttempts, $saveToDatabase = false, $dbPath = null, $playerName = 'Player') {
+        if ($saveToDatabase) {
+            $db = new Database($dbPath);
+        }
+
+        $game = new Game($maxNumber, $maxAttempts, $playerName);
+        $game->play();
+
+        if ($saveToDatabase) {
+            $db->saveGame($game);
+        }
+    }
+}
---
+++ Task04/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9263d1b491df547825b9dfe974c03682bd15ae6f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9263d1b491df547825b9dfe974c03682bd15ae6f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'haiven/guess-number' => array(
+            'pretty_version' => 'v1.2',
+            'version' => '1.2.0.0',
+            'reference' => 'e5f4d4fdcc917a4e0728ceedaa3083ba087d2e9a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../haiven/guess-number',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.1',
+            'version' => '0.12.1.0',
+            'reference' => 'd1fe500378f53fb5ae1072c0daa77095c384a082',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
